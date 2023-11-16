3b337a5aead0616a79604bee12e53c87215392e0
zbychfish <zszmigiero@gmail.com>
2023-11-17 00:16:19 +0100

    modified:   init-new.sh

--- init-new.sh
+++ init-new.sh
@@ -42,6 +42,7 @@ get_credentials
 #get_certificates
 save_variable GI_ICS_IN "N"
 save_variable GI_OCP_IN "N"
+save_variable GI_IN "N"
 [[ "$gi_install" == 'Y' ]] && get_gi_options
 [[ "$ics_install" == 'Y' || "$gi_install" == 'Y' ]] && get_ics_options
 #[[ "$cp4s_install" == 'Y' ]] && get_cp4s_options
