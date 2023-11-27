6a16286a42041f308bd008dac814048280cc1063
zbychfish <zszmigiero@gmail.com>
2023-11-27 19:25:40 +0100

    modified:   prepare-airgap-archives/prepare-air-gap-ics.sh

--- prepare-airgap-archives/prepare-air-gap-ics.sh
+++ prepare-airgap-archives/prepare-air-gap-ics.sh
@@ -11,15 +11,24 @@ get_ics_version_prescript
 ics_version=$(($ics_version-1))
 msg "Access to ICS packages requires RedHat account authentication for some container images" info
 get_account "Insert RedHat account name"
+exit 1
 rh_account=$curr_value
 echo "$rhn_secret" > $GI_TEMP/pull-secret.txt
 curr_value=""
 while $(check_input "txt" "${curr_value}" "non_empty")
 do
-        get_input "pwd" "Insert password for RedHat account $rh_account: " false
+        get_input "txt" "Insert password for RedHat account $rh_account: " false
         curr_value="$input_variable"
 done
 rh_account_pwd=$curr_value
+msg "Access to ICS packages requires IBM Cloud account authentication for some container images" info
+curr_value=""
+while $(check_input "txt" "${curr_value}" "non_empty")
+do
+        get_input "txt" "Insert your IBM Cloud Key: " false
+        curr_value="$input_variable"
+done
+ibm_account_pwd=$curr_value
 msg "Setup mirror image registry ..." task
 setup_local_registry
 msg "Download support tools ..." task
@@ -50,6 +59,7 @@ do
         cloudctl case launch --case $GI_TEMP/ics_offline/${CASE_ARCHIVE} --inventory ${CASE_INVENTORY_SETUP} --action configure-creds-airgap --args "--registry $site --user $rh_account --pass $rh_account_pwd"
 	check_exit_code $? "Cannot configure credentials for site $site"
 done
+cloudctl case launch --case $GI_TEMP/ics_offline/${CASE_ARCHIVE} --inventory ${CASE_INVENTORY_SETUP} --action configure-creds-airgap --args "--registry cp.icr.io --user cp --pass ibm_account_pwd"
 cloudctl case launch --case $GI_TEMP/ics_offline/${CASE_ARCHIVE} --inventory ${CASE_INVENTORY_SETUP} --action configure-creds-airgap --args "--registry `hostname --long`:5000 --user admin --pass guardium"
 cloudctl case launch --case $GI_TEMP/ics_offline/${CASE_ARCHIVE} --inventory ${CASE_INVENTORY_SETUP} --action mirror-images --args "--registry `hostname --long`:5000 --inputDir $GI_TEMP/ics_offline"
 check_exit_code $? "Cannot mirror ICS images"
