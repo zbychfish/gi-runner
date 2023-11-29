75f05b35581060db13a8f70c8b80b8da18e3cd9b
zbychfish <zszmigiero@gmail.com>
2023-11-29 09:04:09 +0100

    modified:   scripts/functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -16,6 +16,7 @@ function get_latest_gi_images () {
 			image_release=`echo "$line" | awk -F ':' '{print $4}' | awk -F '-' '{print $2}'` 
 			cmaj=`echo ${image_release:1} | cut -d "." -f 2`
 			cmaj=`expr $cmaj + 0`
+			echo "$line"
 			echo $cmaj
 			#cmin=`echo ${image_release:1} | cut -d "." -f 3`
 			#cmin=`expr $cmin + 0`
