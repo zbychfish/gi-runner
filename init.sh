a05cb2b3f5607ea98b2e08328e061611815f447e
zbychfish <zszmigiero@gmail.com>
2024-01-31 09:18:37 +0100

    modified:   init.sh 	modified:   playbooks/gi_install.yaml 	modified:   plays/gi_install.yaml

--- init.sh
+++ init.sh
@@ -42,7 +42,7 @@ get_cluster_storage_info
 get_inter_cluster_info
 get_credentials
 get_certificates
-#[[ "$gi_install" == 'Y' ]] && get_gi_options
+[[ "$gi_install" == 'Y' ]] && get_gi_options
 [[ "$ics_install" == 'Y' || "$gi_install" == 'Y' ]] && get_ics_options
 #[[ "$cp4s_install" == 'Y' ]] && get_cp4s_options
 [[ "$install_ldap" == 'Y' ]] && get_ldap_options
