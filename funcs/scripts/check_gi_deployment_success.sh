96afb9d078f35cf408b4b602bdddcf7d7dc7ffc0
zbychfish <zszmigiero@gmail.com>
2024-02-03 21:16:06 +0100

    modified:   funcs/scripts/check_gi_deployment_success.sh

--- funcs/scripts/check_gi_deployment_success.sh
+++ funcs/scripts/check_gi_deployment_success.sh
@@ -1,5 +1,5 @@
 if [[ $(oc get rs -n $1 | grep cp-serviceability | wc -l) -eq 2 ]]
 then
-        oc delete rs -n $1 $(oc get rs -n $1 | grep cp-serviceability | awk '{if ($2$3$4 == 111) {print $1}}')
+        oc delete rs -n $1 $(oc get rs -n $1 | grep cp-serviceability | awk '{print $1}')
 fi
 [[ $(oc get guardiuminsights -n $1 -o json|jq .items[0].status.versions.reconciled|tr -d '"') == "$2" ]] && echo 1 || echo 0
