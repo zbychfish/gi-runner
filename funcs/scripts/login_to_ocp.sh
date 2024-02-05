007642768960f7d1efe0205592a5689290b8a6e1
zbychfish <zszmigiero@gmail.com>
2024-02-05 20:14:44 +0100

    renamed:    scripts/get_ocp_token.py -> funcs/scripts/get_ocp_token.py 	modified:   funcs/scripts/login_to_ocp.sh

--- funcs/scripts/login_to_ocp.sh
+++ funcs/scripts/login_to_ocp.sh
@@ -1,1 +1,1 @@
-oc login --token=`python3 scripts/get_ocp_token.py $GI_OCADMIN $GI_OCADMIN_PWD $GI_DOMAIN` --server=https://api.${GI_DOMAIN}:6443
+oc login --token=`python3 ../funcs/scripts/get_ocp_token.py $GI_OCADMIN $GI_OCADMIN_PWD $GI_DOMAIN` --server=https://api.${GI_DOMAIN}:6443
