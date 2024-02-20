3765dc53260b80dfb28237e34aef42bc1414647f
zbychfish <zszmigiero@gmail.com>
2024-02-20 15:10:47 +0100

    modified:   prepare_offline.sh

--- prepare_offline.sh
+++ prepare_offline.sh
@@ -16,4 +16,4 @@ msg "Airgap prescript summary" task
 msg "In $GI_TEMP/download directory you have:" info
 msg "- the latest gi-runner version (main branch) - gi-runner.zip" info
 msg "- the unzip rpm package to install on target bastion to unpack gi-runner zip archive - unzip-<current_version>.x86_64.rpm" info
-msg "- collected software packages for OS - os-<os_version>-<date_of_creation>.tar
+msg "- collected software packages for OS - os-<os_version>-<date_of_creation>.tar" info
