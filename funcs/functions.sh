77d599e096abb6ecc004b1f283b10f9a767839e6
zbychfish <zszmigiero@gmail.com>
2024-02-21 21:56:29 +0100

    modified:   funcs/functions.sh

--- funcs/functions.sh
+++ funcs/functions.sh
@@ -2859,7 +2859,7 @@ function software_installation_on_online() {
 function software_installation_on_offline() {
         local is_updated
         msg "Update and installation of software packaged" task
-        if [[ `uname -r` != `cat $GI_TEMP/archive/kernel.txt` ]]
+        if [[ `uname -r` != `cat $GI_TEMP/archives/kernel.txt` ]]
         then
                 msg "Kernel of air-gap bastion differs from air-gap file generator!" info
                 msg "In most cases the independent kernel update will lead to problems with system libraries" info
@@ -2873,17 +2873,17 @@ function software_installation_on_offline() {
                         display_error "Upload air-gap files corresponding to bastion kernel or generate files for bastion environment"
                 fi
         fi
-	[[ -d $GI_TEMP/archive/os-updates ]] && { msg "Installing OS updates" task; dnf -qy --disablerepo=* localinstall ${GI_TEMP}/os/os-updates/*rpm --allowerasing; }
+	[[ -d $GI_TEMP/archives/os-updates ]] && { msg "Installing OS updates" task; dnf -qy --disablerepo=* localinstall ${GI_TEMP}/archives/os-updates/*rpm --allowerasing; }
         msg "Installing OS packages" task
-        dnf -qy --disablerepo=* localinstall ${GI_TEMP}/archive/os-packages/*rpm --allowerasing
+        dnf -qy --disablerepo=* localinstall ${GI_TEMP}/archives/os-packages/*rpm --allowerasing
         msg "Installing Ansible and python modules" task
-        cd ${GI_TEMP}/archive/ansible
+        cd ${GI_TEMP}/archives/ansible
         pip3 install passlib-* --no-index --find-links '.' > /dev/null 2>&1
         pip3 install dnspython-* --no-index --find-links '.' > /dev/null 2>&1
         pip3 install beautifulsoup4-* --no-index --find-links '.' > /dev/null 2>&1
         pip3 install argparse-* --no-index --find-links '.' > /dev/null 2>&1
         pip3 install jmespath-* --no-index --find-links '.' > /dev/null 2>&1
-        cd $GI_TEMP/archive/galaxy
+        cd $GI_TEMP/archives/galaxy
         ansible-galaxy collection install community-general-${galaxy_community_general}.tar.gz
         ansible-galaxy collection install ansible-utils-${galaxy_ansible_utils}.tar.gz
         ansible-galaxy collection install community-crypto-${galaxy_community_crypto}.tar.gz
