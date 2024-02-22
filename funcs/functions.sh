29afa28b7268a844cce38b220ff4229f3d2a8149
zbychfish <zszmigiero@gmail.com>
2024-02-22 12:37:42 +0100

    modified:   funcs/functions.sh 	deleted:    scripts/dev/bastion-ip-setup.sh 	deleted:    scripts/functions.sh 	deleted:    scripts/ics-uninstall.sh 	deleted:    scripts/init.globals.sh 	deleted:    scripts/install-tar.sh 	deleted:    scripts/machine_config.yaml 	deleted:    scripts/nfs-client-rbac.yaml 	deleted:    scripts/nfs-client-sc.yaml 	deleted:    scripts/rook-latest-common.yaml 	deleted:    scripts/rook-latest-crds.yaml 	deleted:    scripts/rook-latest-filesystem.yaml 	deleted:    scripts/rook-storage_class_cephfs.yaml 	deleted:    scripts/rook-uninstall.sh 	deleted:    scripts/shared_functions.sh 	deleted:    scripts/upgrade_common_services.sh

--- funcs/functions.sh
+++ funcs/functions.sh
@@ -2407,7 +2407,7 @@ function prepare_offline_bastion() {
                         gi_archives="${input_variable}"
         done
         save_variable GI_ARCHIVES_DIR "'$gi_archives'"
-        #process_offline_archives
+        process_offline_archives
 	software_installation_on_offline
 }
 
