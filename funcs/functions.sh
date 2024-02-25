d20751955aa4c01ae03b1ce46f08638013d51146
zbychfish <zszmigiero@gmail.com>
2024-02-25 10:28:28 +0100

    modified:   funcs/functions.sh

--- funcs/functions.sh
+++ funcs/functions.sh
@@ -728,7 +728,9 @@ function get_cp4s_options() {
         save_variable GI_CP4S_ADMIN "$cp4s_admin"
         msg "Default storage class for CP4S." info
         msg "All CP4S PVC's use RWO access." info
-	[[ $storage_type == 'R' && $rook_deployment_type == 3 ]] && sc_list=("rook-ceph-block" "rook-ceph-block-nr") || sc_list=("rook-ceph-block")
+	msg "$storage_type $rook_deployment_type" info
+	[[ $storage_type == 'R' -a $rook_deployment_type == 3 ]] && sc_list=("rook-ceph-block" "rook-ceph-block-nr") || sc_list=("rook-ceph-block")
+
         [ $storage_type == 'R' ] && sc_list=(${rook_sc[@]}) #|| sc_list=(${ocs_sc[@]})
 	if [ $storage_type == 'R' ]
 	then
