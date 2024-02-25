c17db7354e026b6dc48f34f556317b6c293aa748
zbychfish <zszmigiero@gmail.com>
2024-02-25 13:16:30 +0100

    modified:   funcs/functions.sh 	modified:   playbooks/stage1.yaml 	modified:   plays/bastion_setup.yaml 	modified:   plays/templates/ocp_install_config.j2

--- funcs/functions.sh
+++ funcs/functions.sh
@@ -3031,7 +3031,6 @@ function unset_proxy_settings() {
         then
                 mv -f /etc/dnf/dnf.conf.gi_no_proxy /etc/dnf/dnf.conf
         fi
-	msg "$cp4s_install $edr_install" info
 	[[ $cp4s_install != 'Y' && $edr_install != 'Y' ]] && save_variable GI_PROXY_URL "NO_PROXY"
 }
 
